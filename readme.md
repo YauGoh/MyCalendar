3f6816c4e33dd33cb54317096a1600227082c5e7
Yau Goh Chow <yaugoh.chow@gmail.com>
2022-10-05 20:29:59 +0800

    Initial

---
+++ readme.md
@@ -0,0 +1,15 @@
+# 1.0 App Registration
+
+## 1.1 Creating new App Registraion
+- Under Enterprise Applications
+- + New Application
+- + Create your own application
+- Select - Register an application to integration with Azure AD
+- Select - Accounts in this Organizational Directory only
+
+## 1.2 Setting up Permissions
+
+- Under API Permissions
+    - Add Permissions
+
+# 2.0 Client Configuration
---
+++ src/MyCalendar.Console/Program.cs
@@ -0,0 +1,82 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Azure.Identity;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Graph;
+using MyCalendar.Console.Configurations;
+
+var configuration = new ConfigurationBuilder()
+     .AddJsonFile($"appsettings.json")
+     .Build();
+
+var microsoftGraphConfig = configuration
+    .GetRequiredSection("MicrosoftGraph")
+    .Get<MicrosoftGraphConfig>();
+
+var scopes = new string[]
+{
+    $"{microsoftGraphConfig.ApiUrl}.default"
+};
+
+var clientSecretCredential = new ClientSecretCredential(
+    microsoftGraphConfig.Tenant,
+    microsoftGraphConfig.ClientId,
+    microsoftGraphConfig.ClientSecret,
+    new TokenCredentialOptions
+    {
+        AuthorityHost = AzureAuthorityHosts.AzurePublicCloud
+    });
+
+var graphServiceClient = new GraphServiceClient(clientSecretCredential, scopes);
+
+// Get the user using his Email Address
+
+var users = await graphServiceClient.Users
+    .Request()
+    .Filter("Mail eq 'yauGohChow@HackathonOct22Definitiv.onmicrosoft.com'")
+    .GetAsync();
+
+var user = users.First();
+
+Console.WriteLine($"For the user {user.DisplayName}:");
+
+// Get the Default calendar for the user
+
+var calendars = await graphServiceClient
+    .Users[user.Id]
+    .Calendars
+    .Request()
+    .Filter("name eq 'Calendar'")
+    .GetAsync();
+
+var calendar = calendars.First();
+
+Console.WriteLine($"    found calendar {calendar.Name} ({calendar.Id})");
+
+// Create the Event
+
+var @event = new Event
+{
+    Subject = "Annual leave",
+    Start = new DateTimeTimeZone
+    {
+        DateTime = "2022-10-5T00:00:00",
+        TimeZone = "W. Australia Standard Time"
+    },
+    End = new DateTimeTimeZone
+    {
+        DateTime = "2022-10-6T00:00:00",
+        TimeZone = "W. Australia Standard Time"
+    },
+    IsAllDay = true
+};
+
+@event = await graphServiceClient
+    .Users[user.Id]
+    .Calendars[calendar.Id]
+    .Events
+    .Request()
+    .AddAsync(@event);
+
+Console.WriteLine($"    Event Added {@event.Subject} ({@event.Id})");
+
+Console.ReadLine();
